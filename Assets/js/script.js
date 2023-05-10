ca8410e9bf274beecd90feda90b07647076a579d
elmiquea <120135663+elmiquea@users.noreply.github.com>
2023-05-10 15:32:25 -0400

    Update script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,7 +9,6 @@ $(document).ready(function () {
     function getWeatherOneAPI(a,b) {
         var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + a + "&lon=" + b + "&exclude=minutely,hourly&appid=aec299195260a001b09706b5bfe740f7&units=imperial";
 
-        
         //second API call to get the rest of the current weather data along with the 5 day forecast
         $.ajax({
             url: queryURL2,
